ebc4238f82231a581b8bbc6c91237484d3bdc9da
luc12 <danunike687@gmail.com>
2025-11-23 16:58:52 +0700

    fix: edit FE url and remove double error response in cors middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -58,7 +58,7 @@ func (g *gzipResponseWriter) Write(b []byte) (int, error) {
 
 // CORS Middleware
 
-var  whiteList []string = []string{"http://localhost:3000","https://open-api-to-mcp-server-fe.vercel.app/"}
+var  whiteList []string = []string{"http://localhost:3000","https://open-api-to-mcp-server-fe.vercel.app"}
 
 func CORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -70,7 +70,7 @@ func CORS(next http.Handler) http.Handler {
         } else {
 			w.WriteHeader(http.StatusForbidden) 
            utils.SendError(w,http.StatusForbidden, "Forbidden: Origin not allowed")
-    		w.Write([]byte("Forbidden: Origin not allowed"))
+    		
             return
         }
 
